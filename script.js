c384c8c5070acc5c05b7a47a75c7e9c48279a739
joaoromano06 <joaoromano06@gmail.com>
2024-06-11 07:43:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Menu de Hambúrguer
+const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('nav ul');
+
+menuBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+// Rolagem Suave para Seções
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
